986f28ee36d4160dce78a55dd1455369176c3dab
jerome.renard <devnull@localhost>
2009-05-20 12:10:49 +0000

    - Updated MySQL schema

--- sql/mysql/schema.sql
+++ sql/mysql/schema.sql
@@ -4,6 +4,5 @@ CREATE TABLE urlalias (
 	module varchar(30) CHARACTER SET ascii NOT NULL,
 	view varchar(30) CHARACTER SET ascii NOT NULL,
 	parameters varchar(200) CHARACTER SET ascii NOT NULL,
-	useregex tinyint(1) NOT NULL DEFAULT '0',
 	PRIMARY KEY (source)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
