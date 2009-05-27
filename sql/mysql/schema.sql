a53b3d428105faf4e1738c0e97986f0481ada9e4
jerome.renard <devnull@localhost>
2009-05-27 21:47:37 +0000

    - Updated schema with generic_routes fields

--- sql/mysql/schema.sql
+++ sql/mysql/schema.sql
@@ -5,5 +5,18 @@ CREATE TABLE `urlalias` (
   `module` varchar(30) CHARACTER SET ascii NOT NULL,
   `view` varchar(30) CHARACTER SET ascii NOT NULL,
   `parameters` varchar(200) CHARACTER SET ascii NOT NULL,
-  PRIMARY KEY (`source`)
+-- this flag makes it possible to define a URL alias as
+-- a generic route. If so it will be treated like a regular
+-- expression and will be applied to any incoming URI
+  `generic_route` tinyint(1) NOT NULL DEFAULT '0',
+
+-- the routes will be called in ascending order
+-- a priority of 1 is the highest and a priotiry of 100 
+-- is the lowest
+-- it is only useful for the generic routes sytem
+-- and will not be used for the standard URL alias mapping system
+  `route_priority` tinyint(4) DEFAULT NULL,
+
+  PRIMARY KEY (`source`),
+  UNIQUE KEY `route_priority` (`route_priority`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
