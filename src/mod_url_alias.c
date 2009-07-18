b709fce12e7d6bcc46642bbfa4bcecf3bac0f411
Jerome Renard <jerome.renard@gmail.com>
2009-07-18 13:21:55 +0200

    - Fixed LICENSE header

--- src/mod_url_alias.c
+++ src/mod_url_alias.c
@@ -1,5 +1,5 @@
 /**
- * Copyright [2009] [Jérôme Renard jerome.renard@gmail.com]
+ * Copyright 2009 Jérôme Renard jerome.renard@gmail.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
